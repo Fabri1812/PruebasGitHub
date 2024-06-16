49d8ed5854e544022b22949a20158f740b16df09
Fabri1812 <afabriciochacon28@gmail.com>
2024-06-16 14:18:44 -0600

    Add project files.

---
+++ PruebasGitHub/Models/Mango.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PruebasGitHub.Models
+{
+    public class Mango
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+    }
+}
