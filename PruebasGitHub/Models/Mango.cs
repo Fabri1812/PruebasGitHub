0d984b654386d13aa89ce62b812d4fabab2bc171
Fabri1812 <afabriciochacon28@gmail.com>
2024-06-16 14:23:46 -0600

    rakatanga

--- PruebasGitHub/Models/Mango.cs
+++ PruebasGitHub/Models/Mango.cs
@@ -10,7 +10,7 @@ namespace PruebasGitHub.Models
         [Required]
         public string Nombre { get; set; }
 
-        public string Descripcion { get; set; }
+        public string Penesote { get; set; }
 
     }
 }
