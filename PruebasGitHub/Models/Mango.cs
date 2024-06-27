de8752a42bb8e2f0d00ff8e48569c83bc0f5ba8f
Fabri1812 <afabriciochacon28@gmail.com>
2024-06-26 19:41:41 -0600

    prueba varias variables

--- PruebasGitHub/Models/Mango.cs
+++ PruebasGitHub/Models/Mango.cs
@@ -16,5 +16,15 @@ namespace PruebasGitHub.Models
         public string variable2 { get; set; }
 
         public string variable4 { get; set; }
+        public string qwr { get; set; }
+        public string asq3 { get; set; }
+        public string svew4 { get; set; }
+        public string asfq2 { get; set; }
+        public string asf123 { get; set; }
+        public string asf125t { get; set; }
+        virtual public string hgaght { get; set; }
+        public string sthsr { get; set; }
+        public string gSFga { get; set; }
+        public string vardgsgiable4 { get; set; }
     }
 }
