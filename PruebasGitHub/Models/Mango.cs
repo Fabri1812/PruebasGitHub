1af502e903ae79bb821d9236de65100b67545da1
KendalSalazar <kendalsalazarumana@gmail.com>
2024-06-26 19:31:22 -0600

    rdgdg

--- PruebasGitHub/Models/Mango.cs
+++ PruebasGitHub/Models/Mango.cs
@@ -14,7 +14,6 @@ namespace PruebasGitHub.Models
         public string variable1 { get; set; }
 
         public string variable2 { get; set; }
-        public string variable3 { get; set; }
 
         public string variable4 { get; set; }
     }
